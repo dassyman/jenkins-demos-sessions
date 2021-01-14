ee709398dd9e033512b952c658bd898debcf2859
Poulloura, Anastasis <anastasis.poulloura@roche.com>
2021-01-14 12:59:21 +0100

    new jenkinsfile

--- test.sh
+++ test.sh
@@ -1,5 +1,2 @@
-String getUserName(String firstName, String lastName) {
-    return firstName.substring(0, 1).toLowerCase() + lastName.toLowerCase();
-} 
-
-assert getUserName("Jai", "Salas") == "jsalas" : "getUserName is not working";
+#!/bin/sh
+echo "Inside the script, demo $DEMO"
